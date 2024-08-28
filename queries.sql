8c1e71e67eabf27db0219bf4776cf00e915216c6
IgorKiryanov-2024 <Investsov@yandex.ru>
2024-08-28 12:45:42 +0500

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,10 +1,11 @@
---1. Считаем общее количество покупателей из таблицы customers.
--- Используем агрегирующий оператор COUNT - SQL функция используется для подсчета количества строк. 
+--1.Считаем общее количество покупателей из таблицы customers.
+-- Используем COUNT-используется для подсчета количества строк. 
 select count(customer_id) as customers_count
 from customers;
---2.1. Первый отчет о десятке лучших продавцов.
---Используем агрегирующий оператор COUNT - SQL функция используется для подсчета количества строк.
---Используем агрегирующий оператор SUM - SQL функция используется для суммирования строк (количество умножаем на цену товара). Оператор FLOOR - для округления целого числа
+--2.1.Первый отчет о десятке лучших продавцов.
+--Используем COUNT-спользуется для подсчета количества строк.
+--Используем SUM - используется для суммирования строк
+--Оператор FLOOR - для округления целого числа
 select
     employees.first_name || ' ' || employees.last_name as seller,
     count(employees.employee_id) as operations,
@@ -16,9 +17,10 @@ left join employees on sales.sales_person_id = employees.employee_id
 group by seller
 order by sum(sales.quantity * products.price) desc
 limit 10;
---2.2. Второй отчет - информация о продавцах, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам.
---Используем агрегирующий оператор AVG - SQL функция используется для подсчета среднего значения (количество умножаем на цену товара). Оператор FLOOR - для округления чисел
---Используем агрегирующий оператор having для дополнительной группировки после group by
+--2.2.Второй отчет
+--Используем AVG-используется для подсчета среднего значения
+--Оператор FLOOR-для округления чисел
+--Используем having для дополнительной группировки
 select
     employees.first_name || ' ' || employees.last_name as seller,
     floor(avg(sales.quantity * products.price)) as average_income
@@ -33,9 +35,9 @@ having
         join products on sales.product_id = products.product_id
     )
 order by average_income;
---2.3. Третий отчет - информация о выручке по дням недели.
---Оператор To_char - преобразование даты в день недели
--- Оператор Extract необходим для преобразование текста даты в число
+--2.3.Третий отчет-информация о выручке по дням недели.
+--Оператор To_char-преобразование даты в день недели
+--Оператор Extract необходим для преобразование текста даты в число
 select
     employees.first_name || ' ' || employees.last_name as seller,
     to_char(sales.sale_date, 'day') as day_of_week,
@@ -45,9 +47,9 @@ left join products on sales.product_id = products.product_id
 left join employees on sales.sales_person_id = employees.employee_id
 group by extract('isodow' from sales.sale_date), seller, day_of_week
 order by extract('isodow' from sales.sale_date), seller;
---3.1. Первый отчет - количество покупателей в разных возрастных группах: 16-25, 26-40 и 40+.
---оператор case позволяет осуществить проверку условий и возвратить в зависимости от выполнения того или иного условия тот или иной результат.
---Используем агрегирующий оператор COUNT - SQL функция используется для подсчета количества строк.
+--3.1.Первый отчет-количество покупателей в разных возрастных группах
+--оператор case позволяет осуществить проверку условий и возвратить.
+--Используем COUNT-используется для подсчета количества строк.
 select
     case
         when age between 16 and 25 then '16-25'
@@ -58,10 +60,11 @@ select
 from customers
 group by age_category
 order by age_category;
---3.2. Второй отчет - данные по количеству уникальных покупателей и выручке, которую они принесли.
---Оператор To_char - преобразование даты в день недели
---Используем агрегирующий оператор COUNT - SQL функция используется для подсчета количества строк.
---Используем агрегирующий оператор SUM - SQL функция используется для суммирования строк (количество умножаем на цену товара). Оператор FLOOR - для округления чисел
+--3.2.Второй отчет-данные по количеству уникальных покупателей и выручке
+--Оператор To_char-преобразование даты в день недели
+--Используем COUNT-используется для подсчета количества строк.
+--Используем SUM-используется для суммирования строк
+--Оператор FLOOR-для округления чисел
 select
     to_char(sales.sale_date, 'YYYY-MM') as selling_month,
     count(distinct sales.customer_id) as total_customers,
@@ -72,7 +75,7 @@ left join products on sales.product_id = products.product_id
 left join employees on sales.sales_person_id = employees.employee_id
 group by selling_month
 order by selling_month asc;
---3.3. Третий отчет - о покупателях, первая покупка которых была в ходе проведения акций (акционные товары отпускали со стоимостью равной 0).
+--3.3.Третий отчет o ходе проведения акций.
 --ROW_NUMBER() используем для нумерования строк
 with tab as (
     select
