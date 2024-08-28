22eb118a5cbafd2ffd1eebda55654028031d3bbc
IgorKiryanov-2024 <Investsov@yandex.ru>
2024-08-28 10:22:06 +0500

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,9 +1,9 @@
-1. Считаем общее количество покупателей из таблицы customers.
+--1. Считаем общее количество покупателей из таблицы customers.
    Используем агрегирующий оператор COUNT - SQL функция используется для подсчета количества строк. 
      SELECT COUNT(customer_id) AS customers_count
 FROM customers;
 
-2.1. Первый отчет о десятке лучших продавцов.
+--2.1. Первый отчет о десятке лучших продавцов.
     select
     employees.first_name || ' ' || employees.last_name as seller,
     count(employee_id) as operations, --Используем агрегирующий оператор COUNT - SQL функция используется для подсчета количества строк.
@@ -15,7 +15,7 @@ left join employees on sales.sales_person_id = employees.employee_id
 group by 1
 order by sum(quantity * price) desc
 limit 10;
- 2.2. Второй отчет - информация о продавцах, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам.
+ --2.2. Второй отчет - информация о продавцах, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам.
     select
     employees.first_name || ' ' || employees.last_name as seller,
     FLOOR(AVG(quantity * price)) as average_income --Используем агрегирующий оператор AVG - SQL функция используется для подсчета среднего значения (количество умножаем на цену товара). Оператор ROUND - для округления чисел
@@ -25,7 +25,7 @@ left join employees on sales.sales_person_id = employees.employee_id
 group by 1
 having AVG(quantity * price) < (select AVG(quantity * price) from sales left join products on sales.product_id = products.product_id) --Используем агрегирующий оператор having для дополнительной группировки после group by
 order by 2;
- 2.3. Третий отчет - информация о выручке по дням недели.
+--2.3. Третий отчет - информация о выручке по дням недели.
     select
     employees.first_name || ' ' || employees.last_name as seller,
     --Оператор To_char - преобразование даты в день недели
@@ -38,7 +38,7 @@ left join employees on sales.sales_person_id = employees.employee_id
 group by extract(isodow from sales.sale_date), 1, 2
 order by extract(isodow from sales.sale_date), 2, 1;
    
- 3.1. Первый отчет - количество покупателей в разных возрастных группах: 16-25, 26-40 и 40+.
+ --3.1. Первый отчет - количество покупателей в разных возрастных группах: 16-25, 26-40 и 40+.
     select
     case --оператор позволяет осуществить проверку условий и возвратить в зависимости от выполнения того или иного условия тот или иной результат.
         when age between 16 and 25 then '16-25'
@@ -49,7 +49,7 @@ order by extract(isodow from sales.sale_date), 2, 1;
 from customers
 group by age_category
 order by age_category;
- 3.2. Второй отчет - данные по количеству уникальных покупателей и выручке, которую они принесли.
+ --3.2. Второй отчет - данные по количеству уникальных покупателей и выручке, которую они принесли.
    select
     --Оператор To_char - преобразование даты в день недели
     to_char(sales.sale_date, 'YYYY-MM') as selling_month,
@@ -61,7 +61,7 @@ left join products on sales.product_id = products.product_id
 left join employees on sales.sales_person_id = employees.employee_id
 group by 1
 order by selling_month asc;
- 3.3. Третий отчет - о покупателях, первая покупка которых была в ходе проведения акций (акционные товары отпускали со стоимостью равной 0).
+ --3.3. Третий отчет - о покупателях, первая покупка которых была в ходе проведения акций (акционные товары отпускали со стоимостью равной 0).
     with TAB as (
     select
         CUSTOMERS.CUSTOMER_ID,
